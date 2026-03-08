1db38563ae82a9f7debe02724310328a877c16c0
Anwar <Anwar@DESKTOP-0OH6CLM>
2026-03-09 00:48:34 +0200

    fix: Update Google Client ID in Docker environment configuration

--- CrudTask.Client/src/environments/environment.docker.ts
+++ CrudTask.Client/src/environments/environment.docker.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiUrl: 'http://localhost:5211',
   auth: {
     issuer: 'https://accounts.google.com',
-    clientId: 'YOUR_GOOGLE_CLIENT_ID',
+    clientId: '379286327162-ifjv0jdqn185ko7q6ss9t8fv5aatncjp.apps.googleusercontent.com',
     clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET'
   },
 };
